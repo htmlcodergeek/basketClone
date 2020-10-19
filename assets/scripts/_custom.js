2cff6a97a4040d1ea6428f35f004da9e25a5205b
htmlcodergeek <html.codergeek@gmail.com>
2020-10-19 09:21:03 -0700

    BigBasket Files Inserted

---
+++ assets/scripts/_custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+  //Toggle Class Switch
+    // $('.fa-angle-down').click(function(){
+    //     $(".fa-angle-down").toggleClass("fa-angle-up");
+    // });
+
+   
+   
+});
